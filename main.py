830cd06ac7b051b0c745759f823baa11f04fdb7b
Ayan <ayansingal676@gmail.com>
2025-02-22 11:47:46 -0500

    Tweak to integrate Flask

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
+from flask import Flask, Response
 import cv2
 
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
 
-cap = cv2.VideoCapture(0)
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+@app.route('/video')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if not cap.isOpened():
-    print("Cannot open webcam")
-    exit()
-
-while True:
-    ret, frame = cap.read()
-    
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting...")
-        break
-    
-    cv2.imshow('Webcam', frame)
-    
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-
-print("The world is not ready for Yogai!!!!")
+if __name__ == '__main__':
+    app.run(debug=True)
