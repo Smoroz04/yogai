133acc423c05398bb9405568acc325aa527076e7
Marianna <speranzamarianna04@gmail.com>
2025-04-29 20:26:05 -0400

    photos and new library

--- main.py
+++ main.py
@@ -23,26 +23,35 @@ def generate_frames(poseName):
     frameCounter = 0
     quickTimer = time.time()
     mainTimer = time.time()
-    resetMainTimer = False
     activeAttempt = True
     border_colour = (0, 0, 255)
-
+    clearCSV("joint_angles.csv")
+    currColor = "green"
 
 
     while True:
+        # Open the camera
         success, frame = camera.read()
         if not success:
             break
         else:
+            # opens on specific camera
             frame = cv2.flip(frame, 1)  
+            # Gets the the specific frame 
             frame = detector.findPose(frame)
+            # gets the pose dots from the frame and puts them into an array lmlist
             lmlist = detector.findPosition(frame, draw=False)
             
+            # Gets how long its been since the start of the time
             elapsed_time = time.time() - mainTimer
+
+            # idk what the point of this is tbh
             elapsed_time_text = f"Time: {elapsed_time:.0f}s"  
+
 
             height, width, _ = frame.shape
             thickness = 10
+
             cv2.rectangle(frame, (0,0), (width, height), border_colour, thickness)
 
             cv2.putText(frame, elapsed_time_text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
@@ -52,29 +61,29 @@ def generate_frames(poseName):
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-            if(activeAttempt):
+            if elapsed_time <= 30:
                 currAngles = detector.get_joint_angles(lmlist)
-
                 if currAngles:
-                  save_joint_angles_to_csv(currAngles)
+                    save_joint_angles_to_csv(currAngles)
+                frameCounter += 1
+                quickTimer, changeBorder, currColor = detector.checkPose(quickTimer, currAngles, wantedPose, frameCounter, currColor)
+                if (changeBorder):
+                    # go to red
+                    border_colour = (0,0,255)
+                else:
+                    # Go Green
+                    border_colour = (0,255,0)
+            else:
+                exit
+                
+            
 
 
-                if elapsed_time < 10:
-                    frameCounter += 1
-                    quickTimer, resetMainTimer, changeBorder = detector.checkPose(quickTimer, currAngles, wantedPose, frameCounter)
-
-                    if(changeBorder):
-                        if(border_colour==(0,0,255)):
-                            border_colour = (0,255,0)
-                        else:
-                            border_colour = (0, 0, 255)
-
-                    if resetMainTimer:
-                        mainTimer = time.time()
-                elif elapsed_time >= 10:
-                    print("CONGRATULATIONS")
-                    activeAttempt = False
+def clearCSV(csvName):
+    fileName = csvName
+    f = open(fileName, "w")
+    f.truncate()
+    f.close()
 
 def getPoseData(poseName):
     with open('poseData.csv', mode='r') as csv_data:
@@ -95,7 +104,7 @@ def choose():
          
 @app.route('/video')
 def video_feed():
-    pose_name = request.args.get('pose', 'WarriorPose.png')
+    pose_name = request.args.get('pose', 'mountainPose.png')
     return Response(generate_frames(pose_name), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/display')
